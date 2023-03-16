33ea80868e4dbf6e51173782fad0a5574646b930
SOGIST1 <lbchigges@gmail.com>
2023-03-16 16:50:47 -0400

    Added examples of constants and shadowing.

--- variables_and_mutability/src/main.rs
+++ variables_and_mutability/src/main.rs
@@ -1,6 +1,25 @@
 fn main() {
-    let x = 5;
+    /*
+    // mutability
+    let mut x = 5;
     println!("The value of x is: {x}");
-    x = 6; // This will not compile! x is immutable.
-    println!("The value of x is {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+    */
+
+    /*
+    // constants
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    */
+
+    // shadowing
+    let x = 5;
+
+    let x = x + 1; // x is not mutable, but shadowing is allowed.
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+    println!("The value of x is: {x}");
 }
